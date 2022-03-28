2d71f3be499e11064d868c8939ea52c0ef20ca74
Michael <michael.spengler@sap.com>
2022-03-28 15:09:15 +0200

    prepare demo

--- src/dydx-microservice.ts
+++ src/dydx-microservice.ts
@@ -6,6 +6,12 @@ const port = 4000;
 // Add the bodyParser middelware to the express application
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/getPositions', function (request: any, response: any) {
+
+  return "call getPositions method of dydx-connector.ts"
+
+})
+
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
 });
